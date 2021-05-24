b913823decf15981618e9fe572a3663cec464c8e
Dawlau <blahoviciandrei1@gmail.com>
2021-05-24 11:53:56 +0300

    update: added successors generator

--- README.md
+++ README.md
@@ -12,7 +12,40 @@ current maze
 maze colors
 current boots color
 current boots uses
-back boots color
+backup boots color
 backup boots uses
 
-check in
+generate successors:
+
+validation:
+current boots color == color of next parcel
+current boots usage < 3
+
+
+transition:
+
+move to a valid neighbour and have the next possible options (in maze and current boots color is the same with the color of the neighbour):
+	I) the wizard can either swap current boots with backup boots or not (before entering the parcel) if they are not the same color and same usage and if backup boots exist
+	II) check wether there are some boots the wizard can take on the parcel:
+				1) if not, nothing to be done here
+				2) if there are some boots these are the possible outcomes:
+						- if wizard has no backup boots then just put the parcel boots on backup boots
+						- otherwise: if currentBoots.color != parcelBoots.color then get new node by swapping currentBoots with parcelBoots and then maybe jump to I)
+									 if currentBoots.color == parcelBoots.color and currentBoots.usage > 0 then get new node by swapping currentBoots with parcelBoots and then maybe jump to I)
+
+									 same for backupBoots
+
+
+	for every transition, the usage of current boots increases by one
+	on the new maze, mark the current position of the wizard (with '*') and restore the last position with the original character
+
+costs:
+
+move to parcel of some color => cost is given by the cost of that color
+swap backup boots with current boots => cost is 1
+
+
+goal:
+
+get the magic stone
+get back to the entrance
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import copy
 
+
 class Maze:
 
 	def __init__(self, colorsMaze, maze):
@@ -10,14 +11,23 @@ class Maze:
 		return self.colorsMaze[position[0]][position[1]]
 
 	def getBoots(self, position):
-		i, j = position
-		if self.maze[i][j] == "0":
-			return None
-		return self.maze[i][j]
+		return self.maze[position[0]][position[1]]
 
 	def isValid(self, position):
 		i, j = position
 		return 0 <= i and i < len(self.maze) and 0 <= j and j < len(self.maze[0])
+
+	def setPosition(self, position, color):
+		i, j = position
+		self.maze[i][j] = color
+
+	def __str__(self):
+		res = ""
+
+		for r in self.maze:
+			res += str(r) + "\n"
+
+		return res
 
 
 class Boots:
@@ -37,6 +47,12 @@ class Boots:
 
 	def setUsers(self, uses):
 		self.uses = uses
+
+	def __str__(self):
+		return str(self.color) + " " + str(self.uses) + "\n"
+
+	def __eq__(self, other):
+		return self.color == other.color and self.uses == other.uses
 
 
 class Node:
@@ -58,6 +74,9 @@ class Node:
 
 	def getWizardPosition(self):
 		return self.wizardPosition
+
+	def __str__(self):
+		return self.maze.__str__() + self.currentBoots.__str__() + self.backupBoots.__str__() + "\n" + str(self.wizardPosition)
 
 
 class AstarGraph:
@@ -109,22 +128,185 @@ class AstarGraph:
 				if maze[i][j] == "*":
 					self.startNode = Node(self.init_maze, Boots(colorsMaze[i][j], 1), None, (i, j))
 
-		self.genNextNodes(self.startNode)
+
+	def getMaze(self):
+		return self.init_maze
+
+	def validNode(self, node):
+		return node.getCurrentBoots().getColor() == self.getMaze().getColor(node.getWizardPosition()) and node.getCurrentBoots().getUses() <= 3
 
 	def genNextNodes(self, node):
 
+		successors = []
 		for dir in range(len(self.MOVEROW)):
 
-			nextMaze = copy.deepcopy(node.getMaze())
+			maze = node.getMaze()
 			currentPosition = node.getWizardPosition()
 
 			nextPosition = (currentPosition[0] + self.MOVEROW[dir], currentPosition[1] + self.MOVECOL[dir])
 
-			if nextMaze.isValid(nextPosition):
+			if maze.isValid(nextPosition):
 
-				cost = self.colors[nextMaze.getColor(nextPosition)]
-				print(cost)
+				cost = self.colors[maze.getColor(nextPosition)]
+				nextMaze = copy.deepcopy(maze)
 
-		# for dir in range(len())
+				# nextMaze.setPosition(nextPosition, '*') # move wizard to the next position
+				# nextMaze.setPosition(currentPosition, self.init_maze.getBoots(currentPosition)) # restore the cell with the original value
+
+				# get all boots combinations and try to go to the next position
+
+				currentBoots = node.getCurrentBoots()
+				backupBoots = node.getBackupBoots()
+
+				if nextMaze.getBoots(currentPosition) in self.colors.keys(): # if I have a pair of boots on current position
+
+					parcelBoots = Boots(nextMaze.getBoots(currentPosition), 0)
+					if backupBoots is None: # no backup boots
+
+						backupBoots = copy.deepcopy(parcelBoots)
+
+						newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+						if self.validNode(newNode):
+							successors.append(newNode)
+
+						if currentBoots != backupBoots:
+							currentBoots, backupBoots = backupBoots, currentBoots # swap boots
+
+						newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+						if(self.validNode(newNode)):
+							successors.append(newNode)
+					else:
+
+						if currentBoots != backupBoots:
+							currentBoots, backupBoots = backupBoots, currentBoots
+
+						newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+						if self.validNode(newNode):
+							successors.append(newNode)
+
+						if currentBoots != backupBoots:
+							currentBoots, backupBoots = backupBoots, currentBoots
+
+						if currentBoots.getColor() != parcelBoots.getColor():
+
+							# don't take boots on the parcel
+							newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+							if self.validNode(newNode):
+								successors.append(newNode)
+
+							currentBoots, parcelBoots = parcelBoots, currentBoots # swap out currentBoots
+
+							newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+							if self.validNode(newNode):
+								successors.append(newNode)
+
+							if currentBoots != backupBoots:
+								currentBoots, backupBoots = backupBoots, currentBoots # classic swap
+
+							newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+							if self.validNode(newNode):
+								successors.append(newNode)
+
+							if currentBoots != backupBoots:
+								currentBoots, backupBoots = backupBoots, currentBoots # undo swap
+							currentBoots, parcelBoots = parcelBoots, currentBoots # undo swap
+
+						elif currentBoots.getColor() == parcelBoots.getColor():
+							if currentBoots.getUses() > 0:
+								print("Parcel boots: ", parcelBoots)
+								currentBoots, parcelBoots = parcelBoots, currentBoots # swap out currentBoots
+
+								newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+								if self.validNode(newNode):
+									successors.append(newNode)
+
+								if currentBoots != backupBoots:
+									currentBoots, backupBoots = backupBoots, currentBoots # classic swap
+
+								newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+								if self.validNode(newNode):
+									successors.append(newNode)
+
+								if currentBoots != backupBoots:
+									currentBoots, backupBoots = backupBoots, currentBoots # undo swap
+								currentBoots, parcelBoots = parcelBoots, currentBoots # undo swap
+
+						if backupBoots.getColor() != parcelBoots.getColor():
+
+							# don't take boots on the parcel
+							newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+							if self.validNode(newNode):
+								successors.append(newNode)
+
+							backupBoots, parcelBoots = parcelBoots, backupBoots # swap out backupBoots
+
+							newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+							if self.validNode(newNode):
+								successors.append(newNode)
+
+							if currentBoots != backupBoots:
+								currentBoots, backupBoots = backupBoots, currentBoots # classic swap
+
+							newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+							if self.validNode(newNode):
+								successors.append(newNode)
+
+							if currentBoots != backupBoots:
+								currentBoots, backupBoots = backupBoots, currentBoots # undo swap
+							backupBoots, parcelBoots = parcelBoots, backupBoots # undo swap
+
+						elif backupBoots.getColor() == parcelBoots.getColor():
+							if backupBoots.getUses() > 0:
+								backupBoots, parcelBoots = parcelBoots, backupBoots # swap out backupBoots
+
+								newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+								if self.validNode(newNode):
+									successors.append(newNode)
+
+								if currentBoots != backupBoots:
+									currentBoots, backupBoots = backupBoots, currentBoots # classic swap
+
+								newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+								if self.validNode(newNode):
+									successors.append(newNode)
+
+								if currentBoots != backupBoots:
+									currentBoots, backupBoots = backupBoots, currentBoots # undo swap
+								backupBoots, parcelBoots = parcelBoots, backupBoots # undo swap
+
+				else: # if I don't have a pair of boots on current position
+
+					newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+					if self.validNode(newNode):
+						successors.append(newNode)
+
+					if backupBoots is not None:
+
+						if currentBoots != backupBoots:
+							currentBoots, backupBoots = backupBoots, currentBoots # swap boots
+
+						newNode = Node(nextMaze, Boots(currentBoots.getColor(), currentBoots.getUses() + 1), backupBoots, nextPosition)
+
+						if(self.validNode(newNode)):
+							successors.append(newNode)
+
+		# return successors
+		return list(set(successors))
+
+
 
 graph = AstarGraph("input.txt")
